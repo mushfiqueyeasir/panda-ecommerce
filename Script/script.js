75c017362f6383fe8a475db910571c72bf0394ad
Mushfique Yeasir <mushfiqueyeasir@gmail.com>
2022-02-10 01:37:55 +0600

    css chagne

--- Script/script.js
+++ Script/script.js
@@ -1,15 +1,4 @@
 
-const h2s = document.getElementsByTagName('h2');
-for (const h2 of h2s) {
-    console.log(h2.innerText);
-    h2.style.color = 'lightblue';
-}
-
-const cards = document.getElementsByClassName('card');
-for (const card of cards) {
-    card.style.borderRadius = '30px';
-}
-
 const buyNows = document.getElementsByClassName('btn-buy');
 
 for (const buyNow of buyNows) {
